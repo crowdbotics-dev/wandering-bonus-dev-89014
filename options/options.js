05dd395749f41869284b8ad054cd1dfce73ad46b
Crowdbotics <engineering@crowdbotics.com>
2023-06-06 07:14:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-bonus-dev-89014",
+  url: "https://wandering-bonus-dev-89014.botics.co",
+  api: "https://wandering-bonus-dev-89014.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
